51f14b389bcda69f66874786f57d1961fc6e2256
vyshnaradh06 <vyshnaradh06@gmail.com>
2026-05-14 14:03:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,7 @@
 from flask import Flask
-app = Flask(__name__)
+
+app = Flask(_name_)
 
 @app.route("/")
 def home():
-    return "<h1>Welcome to My Website</h1>"
-
-@app.route("/about")
-def about():
-    return "<h2>About Page</h2>"
-
-if _name_ == "_main_":
-    app.run(host="0.0.0.0", port=8080)
+    return "Hello from Flask on OpenShift!"
